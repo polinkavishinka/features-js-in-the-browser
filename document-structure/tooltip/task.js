24b654927151bcb7cc893c66a0ff7364677801bb
Polina Vyazankina <polinkavishinka@mail.ru>
2022-08-08 18:27:42 +0300

    update

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,34 @@
 let toolips = Array.from(document.querySelectorAll("a.has-tooltip"));
-const divNew = document.createElement("div");
+let divNew = document.createElement("div");
 
+function add_tip(index) {
+        let divNew = document.createElement("div");
+        divNew.classList.add("tooltip");
+        divNew.innerText = toolips[index].title;
+        toolips[index].appendChild(divNew);
+        let coordinates = toolips[index].getBoundingClientRect();
+        console.log(coordinates);
+        divNew.style = `left: ${coordinates.left + 5}px; top: ${ coordinates.top + 20 }px;`;
+}
 
-toolips.forEach((item) => {
-    item.addEventListener("click", (event) => {
-    event.preventDefault();
-    let coordinates = item.getBoundingClientRect();
-    divNew.textContent = item.getAttribute("title");
-    divNew.classList.add("tooltip");
-    divNew.setAttribute(
-            `style`,
-            `left: ${coordinates.left + 5}px; top: ${coordinates.top + 11}px`
-    );
-            item.insertAdjacentElement("afterend", divNew);
-            divNew.classList.toggle("tooltip_active");
-    });
+for (let i = 0; i < toolips.length; i++) {
+        add_tip(i);
+
+        let toolip = toolips[i].querySelector(".tooltip");
+
+        toolips[i].addEventListener("click", function (evtent) {
+                evtent.preventDefault();
+                if (toolip.classList.contains("tooltip_active")) {
+                toolip.classList.remove("tooltip_active");
+        } else {
+                toolips.forEach((element) =>
+                element.querySelector("div.tooltip").classList.remove("tooltip_active")
+        );
+        toolip.classList.add("tooltip_active");
+        }
+        });
+
+        window.addEventListener("scroll", function () {
+        toolip.classList.remove("tooltip_active");
 });
+}
